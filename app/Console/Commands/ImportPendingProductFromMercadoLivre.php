c1e1250439c082163a3ec234597934f1efea52c6
Bruno Caramelo <bruno.caramelo5@gmail.com>
2025-11-30 15:09:46 -0300

    minimal limit ml

--- app/Console/Commands/ImportPendingProductFromMercadoLivre.php
+++ app/Console/Commands/ImportPendingProductFromMercadoLivre.php
@@ -43,7 +43,7 @@ class ImportPendingProductFromMercadoLivre extends Command
         foreach ($pendingItems as $indexPending => $pending) {
 
            if ($indexPending > 0) {
-               $delayToJob->addMinutes(rand(10, 25));
+               $delayToJob->addMinutes(rand(10, 11));
            }
 
             MercadoLivreImportProductByUriAndAttachToProductCentralJob::dispatch($pending)
