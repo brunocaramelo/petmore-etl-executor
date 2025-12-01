775ef485b9e3ef143209b1f9c710af8d059790c2
Bruno Caramelo <bruno.caramelo5@gmail.com>
2025-12-01 20:39:10 -0300

    intervalo menor

--- app/Console/Commands/ModifyContentCopyRight.php
+++ app/Console/Commands/ModifyContentCopyRight.php
@@ -47,7 +47,7 @@ class ModifyContentCopyRight extends Command
 
         foreach ($pendingItems as $indexPending => $pending) {
             if ($indexPending > 0) {
-                $delayToJob->addMinutes(rand(10, 15));
+                $delayToJob->addMinutes(rand(2, 5));
             }
 
             CreateContentWithoutCopyrightJob::dispatch(new CreateRewritedProductAction(), $pending)
