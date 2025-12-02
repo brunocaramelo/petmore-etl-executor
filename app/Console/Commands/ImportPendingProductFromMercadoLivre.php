a6e3392115407175cfe20f45e66d17b770785d3a
Bruno Caramelo <bruno.caramelo5@gmail.com>
2025-12-01 21:43:19 -0300

    ajustes intervalos ml

--- app/Console/Commands/ImportPendingProductFromMercadoLivre.php
+++ app/Console/Commands/ImportPendingProductFromMercadoLivre.php
@@ -43,7 +43,7 @@ class ImportPendingProductFromMercadoLivre extends Command
         foreach ($pendingItems as $indexPending => $pending) {
 
            if ($indexPending > 0) {
-               $delayToJob->addMinutes(rand(30, 45));
+               $delayToJob->addMinutes(rand(15, 25));
            }
 
             MercadoLivreImportProductByUriAndAttachToProductCentralJob::dispatch($pending)
