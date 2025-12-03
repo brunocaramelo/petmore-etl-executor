e378466377d0d9eca815bb697fbefdb055db0194
Bruno Caramelo <bruno.caramelo5@gmail.com>
2025-12-02 23:23:51 -0300

    ajustes informacoes audentes

--- app/Console/Commands/ModifyContentCopyRight.php
+++ app/Console/Commands/ModifyContentCopyRight.php
@@ -40,7 +40,6 @@ class ModifyContentCopyRight extends Command
             ->whereNotNull('url_product_ml')
             ->has('productMl')
             ->where('ai_adapted_the_content', false)
-            ->limit(1)
             ->get();
 
 
