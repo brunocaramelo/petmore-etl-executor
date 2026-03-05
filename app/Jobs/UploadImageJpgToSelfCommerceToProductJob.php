4d2e658fef212e6fbd54c1641599cafd1bbfd6e1
Bruno Caramelo <bruno.caramelo5@gmail.com>
2026-03-05 12:58:44 -0300

    reenvio de produtdos por falta de foto

--- app/Jobs/UploadImageJpgToSelfCommerceToProductJob.php
+++ app/Jobs/UploadImageJpgToSelfCommerceToProductJob.php
@@ -22,16 +22,7 @@ class UploadImageJpgToSelfCommerceToProductJob implements ShouldQueue
     ) {
         $this->productSku = $productSku;
         $this->pathOfImage = $pathOfImage;
-        // $this->consumer = $consumer;
-
-        $this->consumer = new \App\Consumers\SelfEcommerceConsumer(
-                new \App\Consumers\SelfEcommerceAuthConsumer(
-                    config('custom-services.apis.self_ecommerce.admin_username'),
-                    config('custom-services.apis.self_ecommerce.admin_password')
-                ), [
-            'base_path' => config('custom-services.apis.self_ecommerce.base_url'),
-            'auto_login' => true,
-        ]);
+        $this->consumer = $consumer;
 
         $this->onQueue(config('custom-services.jobs_intervals.minutes.send-self-ecommerce.queue'));
 }
