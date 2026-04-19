06702a2822750506a84e9bf3f8505ef31c3e328f
bruno caramelo souza <bruno.caramelo5@gmail.com>
2026-04-19 11:37:31 -0300

    pocentafem com round

--- app/Imports/SupplierProductsPlanGetShippingDataImport.php
+++ app/Imports/SupplierProductsPlanGetShippingDataImport.php
@@ -150,7 +150,7 @@ class SupplierProductsPlanGetShippingDataImport implements ToCollection, WithHea
         $supplierDescontoPercentual = $this->preferOriginData($this->doFloatMoney($row['supplier_desconto_fornecedor']) ?? null, $updateData->supplier_desconto_percentual ?? null);
         $supplierValorFinal = $this->preferOriginData($this->doFloatMoney($row['supplier_valor_final']) ?? null, $updateData->supplier_valor_final ?? null);
         $sellerSugestaoVenda = $this->preferOriginData($this->doFloatMoney($row['seller_sugestao_venda']) ?? null, $updateData->seller_sugestao_venda ?? null);
-        $sellerMarkup = $this->preferOriginData($this->doFloatMoney($row['seller_markup']) ?? null, $updateData->seller_markup ?? null);
+        $sellerMarkup = $this->preferOriginData($this->doRoundToInt($row['seller_markup']) ?? null, $updateData->seller_markup ?? null);
 
         return [
             'has_searched' => true,
