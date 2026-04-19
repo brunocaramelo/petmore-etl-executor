52873d43fbfbe7df2a8b0a8ad6351b9f50da0750
bruno caramelo souza <bruno.caramelo5@gmail.com>
2026-04-19 18:18:47 -0300

    ctestando parse

--- app/Imports/SupplierProductsPlanGetShippingDataImport.php
+++ app/Imports/SupplierProductsPlanGetShippingDataImport.php
@@ -144,7 +144,7 @@ class SupplierProductsPlanGetShippingDataImport implements ToCollection, WithHea
             $updateData->external_supplier_sku ?? null
         );
         $supplierPrecoPadrao = $this->preferOriginData($this->doFloatMoney($row['supplier_preco_padrao']) ?? null, $updateData->supplier_preco_padrao ?? null);
-        $supplierDescontoPercentual = $this->preferOriginData($this->doFloatMoney($row['supplier_desconto_fornecedor']) ?? null, $updateData->supplier_desconto_percentual ?? null);
+        $supplierDescontoPercentual = $this->preferOriginData($this->doFloatMoney($row['supplier_desconto_percentual']) ?? null, $updateData->supplier_desconto_percentual ?? null);
         $supplierValorFinal = $this->preferOriginData($this->doFloatMoney($row['supplier_valor_final']) ?? null, $updateData->supplier_valor_final ?? null);
         $sellerSugestaoVenda = $this->preferOriginData($this->doFloatMoney($row['seller_sugestao_venda']) ?? null, $updateData->seller_sugestao_venda ?? null);
         $sellerMarkup = $this->preferOriginData($this->doRoundToInt($row['seller_markup']) ?? null, $updateData->seller_markup ?? null);
