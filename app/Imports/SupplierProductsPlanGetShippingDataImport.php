23bc654c0f0851ad69743dbd5e10e2dc598ae4bf
bruno caramelo souza <bruno.caramelo5@gmail.com>
2026-04-19 18:22:21 -0300

    ctestando parse

--- app/Imports/SupplierProductsPlanGetShippingDataImport.php
+++ app/Imports/SupplierProductsPlanGetShippingDataImport.php
@@ -80,8 +80,8 @@ class SupplierProductsPlanGetShippingDataImport implements ToCollection, WithHea
 
     public function persistData()
     {
-        dd($this->data);
         foreach ($this->data as $row) {
+            dd('persistData',$row);
             $updateDatas = ProductSelfCommerceData::whereIn('sku', explode(self::PLAN_SKU_SEPARATOR, $row['local_sku']) ?? ['NENHUM_SKU'] )
                                                   ->where('can_update', true)
                                                   ->get();
